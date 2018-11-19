e4f57365ce04a04fa44c5e074ed882b433d9daa4
hnxyhcwdl1003@163.com <@vincent0506#>
2018-11-19 15:43:17 +0800

    增加webjars

--- jeebiz-boot-samples/jeebiz-boot-sample-druid-war/src/main/java/net/jeebiz/boot/demo/setup/config/BootWebMvcConfig.java
+++ jeebiz-boot-samples/jeebiz-boot-sample-druid-war/src/main/java/net/jeebiz/boot/demo/setup/config/BootWebMvcConfig.java
@@ -8,17 +8,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.web.servlet.resource.WebJarsResourceResolver;
 
 @Configuration
 @EnableWebMvc
 public class BootWebMvcConfig implements WebMvcConfigurer {
  
-	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/static/assets/");
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/")
+				.resourceChain(false).addResolver(new WebJarsResourceResolver())
+				.addResolver(new PathResourceResolver());
 	}
- 
 
 }
--- jeebiz-boot-samples/jeebiz-boot-sample-hikaricp/src/main/java/net/jeebiz/boot/demo/setup/config/BootWebMvcConfig.java
+++ jeebiz-boot-samples/jeebiz-boot-sample-hikaricp/src/main/java/net/jeebiz/boot/demo/setup/config/BootWebMvcConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.web.servlet.resource.WebJarsResourceResolver;
 
 @Configuration
 @EnableWebMvc
@@ -18,6 +20,9 @@ public class BootWebMvcConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/static/assets/");
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/")
+				.resourceChain(false).addResolver(new WebJarsResourceResolver())
+				.addResolver(new PathResourceResolver());
 	}
  
 
