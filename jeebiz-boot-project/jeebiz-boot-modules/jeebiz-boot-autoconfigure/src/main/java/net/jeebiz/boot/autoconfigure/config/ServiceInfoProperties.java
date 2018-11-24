063c24d57fcc4d09ad0ec546cd00d7fbb4927c6b
vindell <hnxyhcwdl1003@163.com>
2018-11-24 21:39:32 +0800

    修改

---
+++ jeebiz-boot-project/jeebiz-boot-modules/jeebiz-boot-autoconfigure/src/main/java/net/jeebiz/boot/autoconfigure/config/ServiceInfoProperties.java
@@ -0,0 +1,66 @@
+package net.jeebiz.boot.autoconfigure.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * 服务信息
+ */
+@ConfigurationProperties("server.info")
+public class ServiceInfoProperties {
+	
+	/**
+	 * 服务节点UID: spring-boot-admin 服务端注册ID
+	 */
+	private String uid;
+	/**
+	 * 服务节点名称
+	 */
+	private String name;
+
+	/**
+	 * 服务节点描述
+	 */
+	private String description;
+
+	/**
+	 * 服务节点版本
+	 */
+	private String version;
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	@Override
+	public String toString() {
+		return "ServiceInfo{" + "name='" + name + '\'' + ", version='" + version + '\'' + '}';
+	}
+}
