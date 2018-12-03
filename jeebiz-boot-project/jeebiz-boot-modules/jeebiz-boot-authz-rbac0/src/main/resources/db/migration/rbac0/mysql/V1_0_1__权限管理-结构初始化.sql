53d8788d4d3e59f2f501f750e3ca81a0c5ede139
vindell <hnxyhcwdl1003@163.com>
2018-12-03 22:58:01 +0800

    修改

--- jeebiz-boot-project/jeebiz-boot-modules/jeebiz-boot-authz-rbac0/src/main/resources/db/migration/rbac0/mysql/V1_0_1__权限管理-结构初始化.sql
+++ jeebiz-boot-project/jeebiz-boot-modules/jeebiz-boot-authz-rbac0/src/main/resources/db/migration/rbac0/mysql/V1_0_1__权限管理-结构初始化.sql
@@ -29,6 +29,7 @@ CREATE TABLE `SYS_AUTHZ_ROLE_PERMS` (
   PRIMARY KEY (`R_ID`,`PERMS`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色-权限关系表（角色-菜单-按钮）';
 
+
 -- ----------------------------
 -- Table structure for SYS_AUTHZ_USER_LIST
 -- ----------------------------
@@ -37,16 +38,17 @@ CREATE TABLE `SYS_AUTHZ_USER_LIST` (
   `U_ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
   `U_USERNAME` varchar(100) NOT NULL COMMENT '用户名',
   `U_PASSWORD` varchar(100) NOT NULL COMMENT '用户密码',
-  `U_ALIAS` varchar(50) NOT NULL COMMENT '用户昵称',
+  `U_ALIAS` varchar(50) DEFAULT NULL COMMENT '用户昵称',
   `U_AVATAR` varchar(300) DEFAULT NULL COMMENT '用户头像：图片路径或图标样式',
   `U_SALT` varchar(64) DEFAULT NULL COMMENT '用户密码盐：用于密码加解密',
   `U_SECRET` varchar(128) DEFAULT NULL COMMENT '用户秘钥：用于用户JWT加解密',
-  `U_PHONE` varchar(11) NOT NULL COMMENT '手机号码',
+  `U_PHONE` varchar(11) DEFAULT NULL COMMENT '手机号码',
   `U_EMAIL` varchar(100) DEFAULT NULL COMMENT '邮箱地址',
   `U_REMARK` varchar(255) DEFAULT NULL COMMENT '备注信息',
   `U_STATUS` int(1) DEFAULT NULL COMMENT '用户状态（0:禁用|1:可用|2:锁定）',
   `U_TIME24` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '初始化时间',
-  PRIMARY KEY (`U_ID`)
+  PRIMARY KEY (`U_ID`),
+  UNIQUE KEY `U_USERNAME` (`U_USERNAME`),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户信息表';
 
 -- ----------------------------
