4b01d32045df979d864be5ce01e75d0f915ce42b
wandl <hnxyhcwdl1003@163.com>
2020-01-18 22:06:40 +0800

    优化第三方以来时候的初始化

--- jeebiz-boot-project/jeebiz-boot-modules/jeebiz-boot-autoconfigure/src/main/java/net/jeebiz/boot/autoconfigure/DefaultSpringContextConfiguration.java
+++ jeebiz-boot-project/jeebiz-boot-modules/jeebiz-boot-autoconfigure/src/main/java/net/jeebiz/boot/autoconfigure/DefaultSpringContextConfiguration.java
@@ -6,9 +6,11 @@ package net.jeebiz.boot.autoconfigure;
 
 import org.springframework.biz.context.SpringContextAwareContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan({"net.jeebiz.**.setup", "net.jeebiz.**.service", "net.jeebiz.**.aspect", "net.jeebiz.**.task"})
 public class DefaultSpringContextConfiguration {
 	
 	@Bean
