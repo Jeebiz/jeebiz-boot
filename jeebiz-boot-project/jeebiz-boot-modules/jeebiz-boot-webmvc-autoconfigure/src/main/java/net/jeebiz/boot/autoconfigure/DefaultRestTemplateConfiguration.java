b6c4c3bcaf65c23ec0a07763cd8d4996aeeda842
wandl <hiwepy@163.com>
2020-08-11 15:30:49 +0800

    xg

--- jeebiz-boot-project/jeebiz-boot-modules/jeebiz-boot-webmvc-autoconfigure/src/main/java/net/jeebiz/boot/autoconfigure/DefaultRestTemplateConfiguration.java
+++ jeebiz-boot-project/jeebiz-boot-modules/jeebiz-boot-webmvc-autoconfigure/src/main/java/net/jeebiz/boot/autoconfigure/DefaultRestTemplateConfiguration.java
@@ -12,6 +12,7 @@ import java.util.List;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +33,7 @@ import com.google.common.collect.Lists;
 import net.jeebiz.boot.autoconfigure.RestTemplateProperties.ClientType;
 
 @Configuration
+@EnableConfigurationProperties(RestTemplateProperties.class)
 public class DefaultRestTemplateConfiguration {
 
 	@Bean
