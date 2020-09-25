c63e24a7c7c13a5f58060d49daa2ee58b9622252
wandl <hnxyhcwdl1003@163.com>
2020-09-25 21:02:59 +0800

    优化

---
+++ jeebiz-boot-project/jeebiz-boot-modules/jeebiz-boot-api/src/main/java/net/jeebiz/boot/api/XHeaders.java
@@ -0,0 +1,17 @@
+package net.jeebiz.boot.api;
+
+public class XHeaders {
+	
+	/**
+	 * HTTP Authorization header, equal to <code>X-Authorization</code>
+	 */
+	public static final String AUTHORIZATION_HEADER = "X-Authorization";
+	public static final String X_LONGITUDE_HEADER = "X-Longitude";
+	public static final String X_LATITUDE_HEADER = "X-Latitude";
+	public static final String X_PROVINCE_HEADER = "X-Province";
+	public static final String X_CITY_HEADER = "X-City";
+	public static final String X_AREA_HEADER = "X-Area";
+	public static final String X_APP_VERSION = "X-APP-VERSION";
+	public static final String X_APP_ID = "X-APP-ID";
+	
+}
