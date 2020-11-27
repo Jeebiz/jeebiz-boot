8407ffb5ded6214a14634724334e7fadab2b90f3
wandl <hiwepy@163.com>
2020-11-27 16:06:02 +0800

    自定义头

--- jeebiz-boot-project/jeebiz-boot-modules/jeebiz-boot-api/src/main/java/net/jeebiz/boot/api/XHeaders.java
+++ jeebiz-boot-project/jeebiz-boot-modules/jeebiz-boot-api/src/main/java/net/jeebiz/boot/api/XHeaders.java
@@ -1,19 +1,67 @@
 package net.jeebiz.boot.api;
 
 public class XHeaders {
-	
-	/**
-	 * HTTP Authorization header, equal to <code>X-Authorization</code>
-	 */
-	public static final String AUTHORIZATION_HEADER = "X-Authorization";
-	public static final String X_LONGITUDE_HEADER = "X-Longitude";
-	public static final String X_LATITUDE_HEADER = "X-Latitude";
-	public static final String X_PROVINCE_HEADER = "X-Province";
-	public static final String X_CITY_HEADER = "X-City";
-	public static final String X_AREA_HEADER = "X-Area";
 
-	public static final String X_APP_CHANNEL = "X-APP-CHANNEL";
-	public static final String X_APP_VERSION = "X-APP-VERSION";
-	public static final String X_APP_ID = "X-APP-ID";
-	
+	/**
+	 * JWT Token
+	 */
+	public static final String X_AUTHORIZATION_HEADER = "X-Authorization";
+	/**
+	 * 经度
+	 */
+	public static final String X_LONGITUDE_HEADER = "X-Longitude";
+	/**
+	 * 纬度
+	 */
+	public static final String X_LATITUDE_HEADER = "X-Latitude";
+	/**
+	 * 最新定位省份
+	 */
+	public static final String X_PROVINCE_HEADER = "X-Province";
+	/**
+	 * 最新定位城市
+	 */
+	public static final String X_CITY_HEADER = "X-City";
+	/**
+	 * 最新定位区
+	 */
+	public static final String X_AREA_HEADER = "X-Area";
+	/**
+	 * 国际化（zh_CN：简体中文、zh_TW：繁体中文、en_US：英语）
+	 */
+	public static final String X_LANGUAGE_HEADER = "X-Language";
+	/**
+	 * 账户UID
+	 */
+	public static final String X_UID_HEADER = "X-Uid";
+	/**
+	 * 参数签名
+	 */
+	public static final String X_SIGN_HEADER = "X-Sign";
+	/**
+	 * 客户端应用ID
+	 */
+	public static final String X_APP_ID_HEADER = "X-APP-ID";
+	/**
+	 * 客户端应用渠道
+	 */
+	public static final String X_APP_CHANNEL_HEADER = "X-APP-CHANNEL";
+	/**
+	 * 客户端应用版本号
+	 */
+	public static final String X_APP_VERSION_HEADER = "X-APP-VERSION";
+	/**
+	 * 接口请求来源（用于行为统计）
+	 */
+	public static final String X_REFERRER_HEADER = "X-Referrer";
+	/**
+	 * 客户端设备唯一标识
+	 */
+	public static final String X_DEVICE_IMEI_HEADER = "X-Device-IMEI";
+	/**
+	 * 客户端设备型号
+	 */
+	public static final String X_DEVICE_MODEL_HEADER = "X-Device-Model";
+
 }
+
