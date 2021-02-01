40bc67347e7cbf2443ae95575db9e734d4b06386
wandl <hnxxyhcwdl1003@163.com>
2021-02-01 22:24:19 +0800

    代理示例

---
+++ jeebiz-boot-modules/jeebiz-boot-plugin-api/src/main/java/net/jeebiz/boot/plugin/api/utils/CglibProxy.java
@@ -0,0 +1,51 @@
+package net.jeebiz.boot.plugin.api.utils;
+
+import java.lang.reflect.Method;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+//Cglib动态代理，实现MethodInterceptor接口
+public class CglibProxy implements MethodInterceptor {
+
+    private Object target;//需要代理的目标对象
+
+    public CglibProxy(Object target) {
+        this.target = target;
+    }
+
+    //重写拦截方法
+    @Override
+    public Object intercept(Object obj, Method method, Object[] arr, MethodProxy proxy) throws Throwable {
+        System.out.println("Cglib动态代理，监听开始！");
+        Object invoke = method.invoke(target, arr);//方法执行，参数：target 目标对象 arr参数数组
+        System.out.println("Cglib动态代理，监听结束！");
+        return invoke;
+    }
+
+    // 定义获取代理对象方法
+    public static Object getCglibProxy(Object objectTarget) {
+        //为目标对象target赋值
+        CglibProxy target = new CglibProxy(objectTarget);
+        Enhancer enhancer = new Enhancer();
+        //设置父类,因为Cglib是针对指定的类生成一个子类，所以需要指定父类
+        enhancer.setSuperclass(objectTarget.getClass());
+        enhancer.setCallback(target);// 设置回调
+        Object result = enhancer.create();//创建并返回代理对象
+        return result;
+    }
+
+    // 定义获取代理对象方法
+    public static <T> T getCglibProxy(Class<T> className) {
+        CglibProxy target = new CglibProxy(BeanUtils.instantiateClass(className));
+        Enhancer enhancer = new Enhancer();
+        //设置父类,因为Cglib是针对指定的类生成一个子类，所以需要指定父类
+        enhancer.setSuperclass(className);
+        enhancer.setCallback(target);// 设置回调
+        Object result = enhancer.create();//创建并返回代理对象
+        return (T) result;
+    }
+
+}
---
+++ jeebiz-boot-modules/jeebiz-boot-plugin-api/src/main/java/net/jeebiz/boot/plugin/api/utils/JdkProxy.java
@@ -0,0 +1,41 @@
+package net.jeebiz.boot.plugin.api.utils;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.cglib.proxy.Enhancer;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+//JDK动态代理实现InvocationHandler接口
+public class JdkProxy implements InvocationHandler {
+
+    private Object target;//需要代理的目标对象
+
+    public JdkProxy(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("JDK动态代理，监听开始！");
+        Object result = method.invoke(target, args);
+        System.out.println("JDK动态代理，监听结束！");
+        return result;
+    }
+
+
+    //定义获取代理对象方法
+    public static Object getJDKProxy(Object targetObject) {
+        //JDK动态代理只能针对实现了接口的类进行代理，newProxyInstance 函数所需参数就可看出
+        return Proxy.newProxyInstance(targetObject.getClass().getClassLoader(), targetObject.getClass().getInterfaces(), new JdkProxy(targetObject));
+    }
+
+    // 定义获取代理对象方法
+    public static <T> T getJDKProxy(Class<T> className) {
+        JdkProxy target = new JdkProxy(BeanUtils.instantiateClass(className));
+        //JDK动态代理只能针对实现了接口的类进行代理，newProxyInstance 函数所需参数就可看出
+        return (T) Proxy.newProxyInstance(className.getClassLoader(), className.getInterfaces(), target);
+    }
+
+}
