cc73d95d9bca61a36bd37aa5c8d226de41f2446c
wandl <hnxxyhcwdl1003@163.com>
2021-05-22 17:01:31 +0800

    优化

---
+++ jeebiz-boot-modules/jeebiz-boot-api/src/main/java/net/jeebiz/boot/api/dao/entities/BaseBean.java
@@ -0,0 +1,36 @@
+package net.jeebiz.boot.api.dao.entities;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+@SuppressWarnings("serial")
+public abstract class BaseBean implements Cloneable, Serializable {
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+    @SuppressWarnings("unchecked")
+	public <T extends BaseBean> T shallowClone() {
+        try {
+            return (T)clone();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
