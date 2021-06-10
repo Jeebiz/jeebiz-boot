dd3fab13314c0f8d3aa81fdea25e22dcefcc5a00
wandl <hiwepy@163.com>
2021-06-10 10:42:49 +0800

    jackson 数据脱敏

---
+++ jeebiz-boot-modules/jeebiz-boot-webmvc-autoconfigure/src/main/java/net/jeebiz/boot/autoconfigure/jackson/Sensitive.java
@@ -0,0 +1,18 @@
+package net.jeebiz.boot.autoconfigure.jackson;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@JacksonAnnotationsInside
+@JsonSerialize(using = SensitiveJsonSerializer.class)
+public @interface Sensitive {
+	
+    SensitiveStrategy strategy();
+}
