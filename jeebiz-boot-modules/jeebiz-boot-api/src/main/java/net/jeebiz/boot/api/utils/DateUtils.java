7e80e8e72ee428adfaee570569eeac9b7cab3693
wandl <hiwepy@163.com>
2021-09-03 10:51:11 +0800

    日期计算

--- jeebiz-boot-modules/jeebiz-boot-api/src/main/java/net/jeebiz/boot/api/utils/DateUtils.java
+++ jeebiz-boot-modules/jeebiz-boot-api/src/main/java/net/jeebiz/boot/api/utils/DateUtils.java
@@ -6,8 +6,34 @@ import java.util.Date;
 public class DateUtils extends hitool.core.lang3.time.DateUtils {
 
 	/**
+	 * 获取前一个分钟值以0或者5结尾的时间点（单位：毫秒）
+	 * [00,05,10,15,20,25,30,35,40,45,50,55]
+	 * @return
+	 */
+	public static long getPreviousMillisEndWithMinute0or5(Date baseTime) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(baseTime);
+		int minute = calendar.get(Calendar.MINUTE);
+		if (minute < 5) {
+			// 当前时间+1小时
+			calendar.set(Calendar.MINUTE, 0);
+			calendar.set(Calendar.SECOND, 0);
+			calendar.set(Calendar.MILLISECOND, 0);
+			return calendar.getTime().getTime();
+		}
+		
+		int minus = minute % 5 < 5 ? minute % 5 : 0;
+
+		calendar.add(Calendar.MINUTE, - minus);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		return calendar.getTime().getTime();
+
+	}
+	
+	/**
 	 * 获取下一个分钟值以0或者5结尾的时间点（单位：毫秒）
-	 * 
+	 * [00,05,10,15,20,25,30,35,40,45,50,55]
 	 * @return
 	 */
 	public static long getNextMillisEndWithMinute0or5(Date baseTime) {
@@ -29,31 +55,55 @@ public class DateUtils extends hitool.core.lang3.time.DateUtils {
 		Date endTime = DateUtils.addHours(calendar.getTime(), 1);
 		return endTime.getTime();
 	}
-
+	
 	/**
-	 * 获取前一个分钟值以0或者5结尾的时间点（单位：毫秒）
-	 * 
+	 * 获取前一个分钟值以0结尾的时间点（单位：毫秒）
+	 * [00,10,20,30,40,50]
 	 * @return
 	 */
-	public static long getPreviousMillisEndWithMinute0or5(Date baseTime) {
+	public static long getPreviousMillisEndWithMinute0(Date baseTime) {
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(baseTime);
 		int minute = calendar.get(Calendar.MINUTE);
-		if (minute < 5) {
+		if (minute < 10) {
 			// 当前时间+1小时
 			calendar.set(Calendar.MINUTE, 0);
 			calendar.set(Calendar.SECOND, 0);
 			calendar.set(Calendar.MILLISECOND, 0);
 			return calendar.getTime().getTime();
 		}
-
-		int add = minute % 5 < 5 ? minute % 5 : 0;
-
-		calendar.add(Calendar.MINUTE, -add);
+		
+		int minus = minute % 10 == 0 ? 10 : minute % 10;
+		
+		calendar.add(Calendar.MINUTE, - minus);
 		calendar.set(Calendar.SECOND, 0);
 		calendar.set(Calendar.MILLISECOND, 0);
 		return calendar.getTime().getTime();
 
+	}
+	
+	/**
+	 * 获取下一个分钟值以0结尾的时间点（单位：毫秒）
+	 * [00,10,20,30,40,50]
+	 * @return
+	 */
+	public static long getNextMillisEndWithMinute0(Date baseTime) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(baseTime);
+		int minute = calendar.get(Calendar.MINUTE);
+		if (minute < 50) {
+			int mod = minute % 10;
+			int add = mod < 10 ? 10 - mod : 0;
+			calendar.add(Calendar.MINUTE, add);
+			calendar.set(Calendar.SECOND, 0);
+			calendar.set(Calendar.MILLISECOND, 0);
+			return calendar.getTime().getTime();
+		}
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		Date endTime = DateUtils.addHours(calendar.getTime(), 1); // 当前小时+1
+		return endTime.getTime();
 	}
 	
 }
--- jeebiz-boot-modules/jeebiz-boot-api/src/test/java/net/jeebiz/boot/api/DateUtils_Test.java
+++ jeebiz-boot-modules/jeebiz-boot-api/src/test/java/net/jeebiz/boot/api/DateUtils_Test.java
@@ -16,39 +16,88 @@ public class DateUtils_Test {
 		
 		try {
 			
-			final String format = "yyyy-MM-dd HH:mm:ss";
-			final String format2 = "yyyyMMdd HHmm";
-			DateFormat dF = new SimpleDateFormat(format);
-			long previous = DateUtils.getPreviousMillisEndWithMinute0or5(new Date());
-			System.out.println(DateFormatUtils.format(new Date(), format) + " > " +  DateFormatUtils.format(previous, format2));
+			testMillisEndWithMinute0();
 			
-			long next = DateUtils.getNextMillisEndWithMinute0or5(new Date());
-			System.out.println(DateFormatUtils.format(new Date(), format) + " > " +  DateFormatUtils.format(next, format2));
+			//testMillisEndWithMinute0or5();
 			
-
-			long test2 = DateUtils.getNextMillisEndWithMinute0or5(dF.parse("2020-07-10 11:02:00"));
-			System.out.println("2020-07-10 11:02:00 > " + DateFormatUtils.format(test2, format2));
-
-			// 跨小时
-			long test3 = DateUtils.getNextMillisEndWithMinute0or5(dF.parse("2020-07-10 12:59:59"));
-			System.out.println("2020-07-10 12:59:59 > " + DateFormatUtils.format(test3, format2));
-			
-			// 跨天
-			long test4 = DateUtils.getNextMillisEndWithMinute0or5(dF.parse("2020-07-10 23:59:59"));
-			System.out.println("2020-07-10 23:59:59 > " + DateFormatUtils.format(test4, format2));
-
-			// 跨闰月
-			long test5 = DateUtils.getNextMillisEndWithMinute0or5(dF.parse("2020-02-29 23:55:59"));
-			System.out.println("2020-02-29 23:55:59 > " + DateFormatUtils.format(test5, format2));
-
-			// 跨闰年
-			long test6 = DateUtils.getNextMillisEndWithMinute0or5(dF.parse("2019-12-31 23:55:59"));
-			System.out.println("2019-12-31 23:55:59 > " + DateFormatUtils.format(test6, format2));
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
         		
+	}
+
+	public static void testMillisEndWithMinute0() throws ParseException {
+		
+		final String format = "yyyy-MM-dd HH:mm:ss";
+		final String format2 = "yyyyMMdd HHmm";
+		DateFormat dF = new SimpleDateFormat(format);
+		long previous = DateUtils.getPreviousMillisEndWithMinute0(new Date());
+		System.out.println(DateFormatUtils.format(new Date(), format) + " > " +  DateFormatUtils.format(previous, format2));
+		
+		long next = DateUtils.getNextMillisEndWithMinute0(new Date());
+		System.out.println(DateFormatUtils.format(new Date(), format) + " > " +  DateFormatUtils.format(next, format2));
+		
+
+		long test2 = DateUtils.getPreviousMillisEndWithMinute0(dF.parse("2020-07-10 11:05:00"));
+		System.out.println("2020-07-10 11:05:00 > " + DateFormatUtils.format(test2, format2));
+
+		long test22 = DateUtils.getPreviousMillisEndWithMinute0(dF.parse("2020-07-10 11:05:00"));
+		System.out.println("2020-07-10 11:05:00 > " + DateFormatUtils.format(test22, format2));
+
+		// 跨小时
+		long test3 = DateUtils.getPreviousMillisEndWithMinute0(dF.parse("2020-07-10 12:59:59"));
+		System.out.println("2020-07-10 12:59:59 > " + DateFormatUtils.format(test3, format2));
+		
+		// 跨天
+		long test4 = DateUtils.getPreviousMillisEndWithMinute0(dF.parse("2020-07-10 23:59:59"));
+		System.out.println("2020-07-10 23:59:59 > " + DateFormatUtils.format(test4, format2));
+
+		// 跨闰月
+		long test5 = DateUtils.getPreviousMillisEndWithMinute0(dF.parse("2020-02-29 23:55:59"));
+		System.out.println("2020-02-29 23:55:59 > " + DateFormatUtils.format(test5, format2));
+
+		// 跨闰年
+		long test6 = DateUtils.getPreviousMillisEndWithMinute0(dF.parse("2019-12-31 23:55:59"));
+		System.out.println("2019-12-31 23:55:59 > " + DateFormatUtils.format(test6, format2));
+		
+	}
+	
+
+	public static void testMillisEndWithMinute0or5() throws ParseException {
+		
+		final String format = "yyyy-MM-dd HH:mm:ss";
+		final String format2 = "yyyyMMdd HHmm";
+		DateFormat dF = new SimpleDateFormat(format);
+		long previous = DateUtils.getPreviousMillisEndWithMinute0or5(new Date());
+		System.out.println(DateFormatUtils.format(new Date(), format) + " > " +  DateFormatUtils.format(previous, format2));
+		
+		long next = DateUtils.getNextMillisEndWithMinute0or5(new Date());
+		System.out.println(DateFormatUtils.format(new Date(), format) + " > " +  DateFormatUtils.format(next, format2));
+		
+
+		long test2 = DateUtils.getPreviousMillisEndWithMinute0or5(dF.parse("2020-07-10 11:05:00"));
+		System.out.println("2020-07-10 11:05:00 > " + DateFormatUtils.format(test2, format2));
+
+		long test22 = DateUtils.getNextMillisEndWithMinute0or5(dF.parse("2020-07-10 11:05:00"));
+		System.out.println("2020-07-10 11:05:00 > " + DateFormatUtils.format(test22, format2));
+
+		// 跨小时
+		long test3 = DateUtils.getNextMillisEndWithMinute0or5(dF.parse("2020-07-10 12:59:59"));
+		System.out.println("2020-07-10 12:59:59 > " + DateFormatUtils.format(test3, format2));
+		
+		// 跨天
+		long test4 = DateUtils.getNextMillisEndWithMinute0or5(dF.parse("2020-07-10 23:59:59"));
+		System.out.println("2020-07-10 23:59:59 > " + DateFormatUtils.format(test4, format2));
+
+		// 跨闰月
+		long test5 = DateUtils.getNextMillisEndWithMinute0or5(dF.parse("2020-02-29 23:55:59"));
+		System.out.println("2020-02-29 23:55:59 > " + DateFormatUtils.format(test5, format2));
+
+		// 跨闰年
+		long test6 = DateUtils.getNextMillisEndWithMinute0or5(dF.parse("2019-12-31 23:55:59"));
+		System.out.println("2019-12-31 23:55:59 > " + DateFormatUtils.format(test6, format2));
+		
 	}
 	
 }
