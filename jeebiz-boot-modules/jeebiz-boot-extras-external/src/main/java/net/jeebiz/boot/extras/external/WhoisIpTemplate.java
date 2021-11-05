a30d7c24bea65faf73c4a883b78a5a6efbc51d3f
wandl <hiwepy@163.com>
2021-11-05 22:34:23 +0800

    组件扩展

--- jeebiz-boot-modules/jeebiz-boot-extras-external/src/main/java/net/jeebiz/boot/extras/external/WhoisIpTemplate.java
+++ jeebiz-boot-modules/jeebiz-boot-extras-external/src/main/java/net/jeebiz/boot/extras/external/WhoisIpTemplate.java
@@ -1,6 +1,6 @@
-/** 
+/**
  * Copyright (C) 2018 Jeebiz (http://jeebiz.net).
- * All Rights Reserved. 
+ * All Rights Reserved.
  */
 package net.jeebiz.boot.extras.external;
 
@@ -43,10 +43,10 @@ import okhttp3.Response;
 public class WhoisIpTemplate {
 
 	protected static String NOT_MATCH = "未分配或者内网IP|0|0|0|0";
-	
+
 	@Autowired
 	private OkHttpClient okhttp3Client;
-	
+
 	private final LoadingCache<String, Optional<JSONObject>> REGION_DATA_CACHES = CacheBuilder.newBuilder()
 			// 设置并发级别为8，并发级别是指可以同时写缓存的线程数
 			.concurrencyLevel(8)
@@ -77,7 +77,7 @@ public class WhoisIpTemplate {
 					Response response = okhttp3Client.newCall(request).execute();
 					if (response.isSuccessful()) {
 						String bodyString = response.body().string();
-						log.info(" IP : {} >> Region : {} ", ip, bodyString); 
+						log.info(" IP : {} >> Region : {} ", ip, bodyString);
 						JSONObject jsonObject = JSONObject.parseObject(bodyString);
 						String addr = jsonObject.getString("addr");
 						if (StringUtils.hasText(addr)) {
@@ -88,7 +88,7 @@ public class WhoisIpTemplate {
 				}
 			});
 
-	private static final String GET_COUNTRY_BY_IP_URL = "http://whois.pconline.com.cn/ipJson.jsp";
+	private static final String GET_COUNTRY_BY_IP_URL = "https://whois.pconline.com.cn/ipJson.jsp";
 	// 810000 香港， 820000 澳门 ，710000 台湾， 999999国外
 	private static final String[] SPECIAL_PROVINCE = new String[] { "810000", "820000", "710000", "999999" };
 	private static final String CHINA = "中国";
@@ -108,7 +108,7 @@ public class WhoisIpTemplate {
 	/**
 	 * IP地址解析
 	 * @param ip
-	 * @return {"ip":"110.137.48.237","pro":"","proCode":"999999","city":"","cityCode":"0","region":"","regionCode":"0","addr":" 印度尼西亚","regionNames":"","err":"noprovince"} 
+	 * @return {"ip":"110.137.48.237","pro":"","proCode":"999999","city":"","cityCode":"0","region":"","regionCode":"0","addr":" 印度尼西亚","regionNames":"","err":"noprovince"}
 	 * @throws ExecutionException
 	 */
 	public JSONObject getRegionData(String ip) {
@@ -120,27 +120,27 @@ public class WhoisIpTemplate {
 			return null;
 		}
 	}
-	
+
 	public RegionAddress getRegionAddress(String ip) throws IOException {
 		try {
 			Optional<JSONObject> optional = REGION_DATA_CACHES.get(ip);
 			if(optional.isPresent()) {
-				
+
 				JSONObject regionData = optional.get();
-				
-				log.info(" IP : {} >> Region : {} ", ip, regionData.toJSONString()); 
-				
+
+				log.info(" IP : {} >> Region : {} ", ip, regionData.toJSONString());
+
 				String province = regionData.getString("pro");
 				String city = regionData.getString("city");
 				String addr = StringUtils.trimWhitespace(regionData.getString("addr"));
 				String country = addr;
-				
+
 				if(Stream.of(SPECIAL_REGION).anyMatch(region -> addr.contains(region))) {
 					country = CHINA;
 				}
-				
+
 				log.info(" IP : {} >> Country/Region : {} ", ip, country);
-				
+
 				return new RegionAddress(country, province, city, "", "");
 			}
 			return new RegionAddress(NOT_MATCH.split("\\|"));
@@ -149,27 +149,27 @@ public class WhoisIpTemplate {
 			return new RegionAddress(NOT_MATCH.split("\\|"));
 		}
 	}
-	
+
 	public RegionEnum getRegionByIp(String ip) {
 		try {
-			
+
 			Optional<JSONObject> optional = REGION_DATA_CACHES.get(ip);
 			if(optional.isPresent()) {
-				
+
 				JSONObject regionData = optional.get();
-				log.info(" IP : {} >> Region : {} ", ip, regionData.toJSONString()); 
-				
+				log.info(" IP : {} >> Region : {} ", ip, regionData.toJSONString());
+
 				String addr = StringUtils.trimWhitespace(regionData.getString("addr"));
 				String country = addr;
-				
+
 				Optional<String> regionOptional = Stream.of(SPECIAL_REGION).filter(region -> addr.contains(region)).findFirst();
 				if(regionOptional.isPresent()) {
 					log.info(" IP : {} >> Country/Region : {} ", ip, country);
 					return SPECIAL_REGION_MAP.get(regionOptional.get());
 				}
-				
+
 				log.info(" IP : {} >> Country/Region : {} ", ip, country);
-				
+
 				return RegionEnum.getByCnName(country);
 			}
 			return RegionEnum.UK;
@@ -178,23 +178,23 @@ public class WhoisIpTemplate {
 			return RegionEnum.UK;
 		}
 	}
-	
+
 	public boolean isMainlandIp(String ip) {
 		try {
 			Optional<JSONObject> optional = REGION_DATA_CACHES.get(ip);
 			if(optional.isPresent()) {
-				
+
 				JSONObject regionData = optional.get();
-				log.info(" IP : {} >> Region : {} ", ip, regionData.toJSONString()); 
-				
+				log.info(" IP : {} >> Region : {} ", ip, regionData.toJSONString());
+
 				String proCode = regionData.getString("proCode");
 				if (!StringUtils.hasText(proCode) || SPECIAL_PROVINCE_SET.contains(proCode)) {
 					return false;
 				}
-				
+
 			}
-			
-			
+
+
 			HttpUrl httpUrl = HttpUrl.parse(GET_COUNTRY_BY_IP_URL).newBuilder()
 					.addQueryParameter("json", Boolean.TRUE.toString())
 					.addQueryParameter("ip", ip).build();
@@ -203,7 +203,7 @@ public class WhoisIpTemplate {
 			if (response.isSuccessful()) {
 				log.info("ip{}  对应地区  {}", ip, response);
 				JSONObject jsonObject = JSONObject.parseObject(response.body().string());
-				
+
 			}
 
 		} catch (Exception e) {
