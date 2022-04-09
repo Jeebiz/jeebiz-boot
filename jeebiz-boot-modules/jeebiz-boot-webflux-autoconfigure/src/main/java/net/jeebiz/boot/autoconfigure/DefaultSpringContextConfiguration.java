0bb5cca1c044e119d570bb296b48a951f61cbc70
wandl <hnxxyhcwdl1003@163.com>
2022-04-09 19:41:34 +0800

    yz

--- jeebiz-boot-modules/jeebiz-boot-webflux-autoconfigure/src/main/java/net/jeebiz/boot/autoconfigure/DefaultSpringContextConfiguration.java
+++ jeebiz-boot-modules/jeebiz-boot-webflux-autoconfigure/src/main/java/net/jeebiz/boot/autoconfigure/DefaultSpringContextConfiguration.java
@@ -4,14 +4,12 @@
  */
 package net.jeebiz.boot.autoconfigure;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.biz.context.SpringContextAwareContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
-@MapperScan({"net.jeebiz.**.dao", "net.jeebiz.**repository"})
 @ComponentScan(basePackages = {"net.jeebiz.**.setup", "net.jeebiz.**.service", "net.jeebiz.**.aspect", "net.jeebiz.**.task", "net.jeebiz.**.strategy", "net.jeebiz.**.extras"})
 public class DefaultSpringContextConfiguration {
 
