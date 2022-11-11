51084fa76f2fb6277b0f375c00d4e2c9b9e7c5c3
wandl <hiwepy@163.com>
2022-11-11 12:18:36 +0800

    更新依赖

--- jeebiz-boot-modules/jeebiz-boot-external-autoconfigure/src/main/java/io/hiwepy/boot/autoconfigure/region/NestedRegionTemplate.java
+++ jeebiz-boot-modules/jeebiz-boot-external-autoconfigure/src/main/java/io/hiwepy/boot/autoconfigure/region/NestedRegionTemplate.java
@@ -60,6 +60,7 @@ public class NestedRegionTemplate {
 	}
 
 	public RegionEnum getRegionByIp(String ipAddress) {
+		try {
 		// 1、去除参数两头空白
 		ipAddress = StringUtils.trimWhitespace(ipAddress);
 		if(InetAddressUtils.internalIp(ipAddress)){
@@ -88,10 +89,14 @@ public class NestedRegionTemplate {
 			return regionEnum;
 		}
 		redisOperation.set(redisKey, RegionEnum.UK.getCode2(), Duration.ofMinutes(30));
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		}
 		return RegionEnum.UK;
 	}
 
 	public String getLocationByIp(String ipAddress) {
+		try {
 		// 1、去除参数两头空白
 		ipAddress = StringUtils.trimWhitespace(ipAddress);
 		if(InetAddressUtils.internalIp(ipAddress)){
@@ -108,9 +113,14 @@ public class NestedRegionTemplate {
 		log.debug("Get Location : {} By ipAddress: {} From Ip2Region ", regionAddress, ipAddress);
 		redisOperation.set(redisKey, regionAddress, Duration.ofMinutes(30));
 		return regionAddress;
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		}
+		return NOT_MATCH;
 	}
 
 	public RegionAddress getRegionAddress(String ipAddress) {
+		try {
 		// 1、去除参数两头空白
 		ipAddress = StringUtils.trimWhitespace(ipAddress);
 		if(InetAddressUtils.internalIp(ipAddress)){
@@ -127,14 +137,23 @@ public class NestedRegionTemplate {
 			}
 		}
 		return regionAddress;
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		}
+		return NOT_MATCH_REGION_ADDRESS;
 	}
 
 	public boolean isMainlandIp(String regionCode, String ipAddress) {
+		try {
 		RegionEnum regionEnum = this.getRegionByCode(regionCode);
 		if(!regionEnum.isValidRegion()) {
 			return this.isMainlandIp(ipAddress);
 		}
 		return regionEnum.isChinaMainland();
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		}
+		return Boolean.FALSE;
 	}
 
 	public boolean isMainlandIp(String ipAddress) {
