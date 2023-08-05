6368cb008334119eae41ccb6bb95795e0cc8dd33
wandl <hiwepy@163.com>
2023-08-06 00:08:11 +0800

    feat:模块初始化配置

--- jeebiz-boot-modules/jeebiz-boot-webflux-autoconfigure/src/main/java/io/hiwepy/boot/autoconfigure/webflux/GlobalErrorWebExceptionHandler.java
+++ jeebiz-boot-modules/jeebiz-boot-webflux-autoconfigure/src/main/java/io/hiwepy/boot/autoconfigure/webflux/GlobalErrorWebExceptionHandler.java
@@ -4,7 +4,7 @@
  */
 package io.hiwepy.boot.autoconfigure.webflux;
 
-import org.springframework.boot.autoconfigure.web.ResourceProperties;
+import org.springframework.boot.autoconfigure.web.WebProperties.Resources;
 import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.reactive.error.ErrorAttributes;
@@ -19,9 +19,9 @@ import java.util.Map;
 
 public class GlobalErrorWebExceptionHandler extends AbstractErrorWebExceptionHandler {
 
-    public GlobalErrorWebExceptionHandler(ErrorAttributes errorAttributes, ResourceProperties resourceProperties,
+    public GlobalErrorWebExceptionHandler(ErrorAttributes errorAttributes, Resources resources,
                                           ApplicationContext applicationContext) {
-        super(errorAttributes, resourceProperties, applicationContext);
+        super(errorAttributes, resources, applicationContext);
     }
 
     // 构造函数
@@ -38,4 +38,5 @@ public class GlobalErrorWebExceptionHandler extends AbstractErrorWebExceptionHan
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(BodyInserters.fromValue(errorPropertiesMap));
     }
+
 }
