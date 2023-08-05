e4734fc4ea0da286b090f28137bd7d666333108f
wandl <hiwepy@163.com>
2023-08-05 20:52:01 +0800

    feat:模块初始化配置

--- jeebiz-boot-samples/jeebiz-boot-sample-r2dbc-webflux/src/main/java/io/hiwepy/boot/demo/repository/entities/DemoEntity.java
+++ jeebiz-boot-samples/jeebiz-boot-sample-r2dbc-webflux/src/main/java/io/hiwepy/boot/demo/repository/entities/DemoEntity.java
@@ -2,10 +2,14 @@
  * Copyright (C) 2018 Hiwepy (http://hiwepy.io).
  * All Rights Reserved.
  */
-package io.hiwepy.boot.demo.dao.entities;
+package io.hiwepy.boot.demo.repository.entities;
 
 import io.hiwepy.boot.api.dao.entities.BaseEntity;
+import lombok.Data;
+import lombok.experimental.Accessors;
 
+@Data
+@Accessors(chain = true)
 public class DemoEntity extends BaseEntity<DemoEntity> {
 
     private static final long serialVersionUID = 6189820231775242317L;
@@ -15,29 +19,5 @@ public class DemoEntity extends BaseEntity<DemoEntity> {
     private String name;
 
     private String text;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
 
 }
