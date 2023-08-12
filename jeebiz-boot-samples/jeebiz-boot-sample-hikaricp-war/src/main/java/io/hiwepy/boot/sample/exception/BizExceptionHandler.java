12691129f9c09d0256763f5c6ad45a6ac7215ede
wandl <hiwepy@163.com>
2023-08-12 20:42:29 +0800

    feat: demo模块

--- jeebiz-boot-samples/jeebiz-boot-sample-hikaricp-war/src/main/java/io/hiwepy/boot/sample/exception/BizExceptionHandler.java
+++ jeebiz-boot-samples/jeebiz-boot-sample-hikaricp-war/src/main/java/io/hiwepy/boot/sample/exception/BizExceptionHandler.java
@@ -4,16 +4,10 @@
  */
 package io.hiwepy.boot.sample.exception;
 
-import com.netflix.client.ClientException;
-import io.hiwepy.boot.api.ApiRestResponse;
 import io.hiwepy.boot.autoconfigure.webmvc.ExceptinHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.biz.context.NestedMessageSource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 public class BizExceptionHandler extends ExceptinHandler {
@@ -23,12 +17,12 @@ public class BizExceptionHandler extends ExceptinHandler {
 
 	/**
 	 * 500 (降级熔断)
-	 */
 	@ExceptionHandler({ ClientException.class })
 	@ResponseBody
 	public ResponseEntity<ApiRestResponse<String>> netflixClientException(ClientException ex) {
 		this.logException(ex);
 		return new ResponseEntity<>( BizExceptionCode.SYSTEM_DEPEND_UPGRADING.asResponse(messageSource), HttpStatus.OK);
 	}
+	 */
 
 }
